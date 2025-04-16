3c99f40ec05e45959650b1e312d0f98be849439e
Sasha <91780565+Sasha1602@users.noreply.github.com>
2025-04-17 00:53:15 +0300

    Update main.py

--- Diplom/main.py
+++ Diplom/main.py
@@ -1,8 +1,10 @@
 import logging
 from aiogram import Bot, Dispatcher
 import asyncio
-from bot_handlers import router  # Импортируйте роутер
-from config import TOKEN
+from bot_handlers_FIXED import router  # Импортируйте роутер
+from config import TOKEN, DB_CONFIG
+from aiomysql import create_pool
+from database import set_db_pool  # эту функцию мы создадим в database.py
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
@@ -17,6 +19,16 @@ dp.include_router(router)
 async def main():
     print("Starting the bot...")
     try:
+        pool = await create_pool(
+            host=DB_CONFIG['host'],
+            user=DB_CONFIG['user'],
+            password=DB_CONFIG['password'],
+            db=DB_CONFIG['db'],
+            autocommit=True,
+            minsize=1,  # минимальное количество соединений
+            maxsize=5   # максимальное количество соединений
+        )
+        set_db_pool(pool)  # передаём пул в database.py
         await dp.start_polling(bot)
     except Exception as e:
         logging.error(f"An error occurred: {e}")
