07fc7bfc7441ba106f9e68cc62d3c273717fbaf1
Sasha <91780565+Sasha1602@users.noreply.github.com>
2025-04-17 00:52:07 +0300

    Delete Diplom/config.py

--- Diplom/config.py
+++
@@ -1,9 +0,0 @@
-# Конфигурационные данные
-TOKEN = '7152369678:AAEKJowscqN23VThG9rajFhiM2wgIGcwSok'
-
-DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '16022004',
-    'db': 'playschamp',  # Используйте 'db' вместо 'database'
-}
